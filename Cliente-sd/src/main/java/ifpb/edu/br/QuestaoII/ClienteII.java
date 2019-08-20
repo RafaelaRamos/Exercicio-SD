1016812b973a40b1403cf9ca4029940abeedc901
RafaelaRamos <rafaellapereyra@gmail.com>
2019-08-20 02:11:36 -0300

    Questao 2

---
+++ Cliente-sd/src/main/java/ifpb/edu/br/QuestaoII/ClienteII.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ifpb.edu.br.QuestaoII;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Cliente
+ */
+public class ClienteII {
+
+   
+    private static Random rd = new Random();
+    private static final ArrayList op = new ArrayList(1);
+    private static final ArrayList mensagem = new ArrayList(2);
+    private static final ExecutorService pool=  Executors.newFixedThreadPool(1);
+
+
+    public ClienteII() {
+
+    }
+
+    public static void main(String[] args) throws IOException {
+        for(int i=0;i<10;i++){
+               pool.execute (new Runnable () {
+    public void run () {
+        
+        try {
+            int resposta = 0;
+            //operacoes
+            op.add("soma");
+            op.add("sub");
+            // numeros aleatorios
+            mensagem.add(rd.nextInt(10));
+            mensagem.add(rd.nextInt(10));
+            mensagem.add(op.get(0));
+            
+            Socket socket = null;
+            
+            socket = new Socket("localhost", 10999);
+            ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
+            
+            output.writeObject(mensagem);
+            
+            ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
+            
+            
+            resposta = input.readInt();
+            
+            requestReply(resposta);
+        } catch (IOException ex) {
+            Logger.getLogger(ClienteII.class.getName()).log(Level.SEVERE, null, ex);
+        }
+
+    }
+    
+});}}
+        public  static void requestReply(int resposta) throws IOException {
+      while(true){
+           
+           
+            Socket socket = new Socket("localhost", 10999);
+            ObjectOutputStream output = new ObjectOutputStream(socket.getOutputStream());
+           
+           
+            //novo numero aleatorio
+            mensagem.set(0, rd.nextInt(10));
+            //resposta da requisicao anterior
+            mensagem.set(1, resposta);
+            //operacao aleatoria
+            Collections.shuffle(op);
+            mensagem.set(2, op.get(0));
+            output.writeObject(mensagem);
+            ObjectInputStream input = new ObjectInputStream(socket.getInputStream());
+            resposta = input.readInt();
+            System.out.print(resposta+" \n");
+
+        }
+    }
+}
+        
